0bcdf611cea4a79d98d2e609d6cc5aac548fc81a
Rodsorty <62730868+Rodsorty@users.noreply.github.com>
2024-06-12 13:54:43 -0600

    Update hola.js

--- hola.js
+++ hola.js
@@ -1,13 +1,14 @@
-var http = require('http');
-var server = http.createServer();
- 
-function mensaje(petic, resp) {
-	resp.writeHead(200, {'content-type': 'text/plain'});
-	resp.write('Hola Mundo');
-	resp.end();
-}
-server.on('request', mensaje);
- 
-server.listen(3000, function () {
-  	console.log('La Aplicación está funcionando en el puerto 3000');
+const http = require('http');
+
+const hostname = 'tu_direccion_ip_elastica'; // Reemplaza esto con tu dirección IP elástica
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hola Mundo!\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`La Aplicación está funcionando en http://${hostname}:${port}/`);
 });
